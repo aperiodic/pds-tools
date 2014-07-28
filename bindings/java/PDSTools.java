66e21b245eeab0e96d57efacb1d4fdfa5052a632
aperiodic <dlp@aperiodic.org>
2014-07-28 01:20:05 -0700

    JNI can parse and pass a HashMap back to Java!

--- bindings/java/PDSTools.java
+++ bindings/java/PDSTools.java
@@ -7,16 +7,22 @@ public class PDSTools {
   public static void main(String[] args) {
     String label_name;
     if (args.length > 1) {
-      label_name = args[0];
+      label_name = args[1];
     } else {
       label_name = "samples/mola.lbl";
     }
 
+    System.out.println(System.getProperty("java.library.path"));
     System.loadLibrary("pdstools");
     PDSTools pds_tools = new PDSTools();
     Map<String,String> blank = new LinkedHashMap<String,String>();
     Map<String,String> label = pds_tools.parse(label_name, blank);
 
-    System.out.println("parsed " + label_name);
+    if (label == null) {
+      System.out.println("couldn't parse " + label_name);
+      return;
+    }
+
+    System.out.println("parsed a version " + label.get("PDS_VERSION_ID") + " label from " + label_name);
   }
 }
--- bindings/java/pdstools.c
+++ bindings/java/pdstools.c
@@ -6,6 +6,8 @@
 #include "../../src/cfg.h"
 #include "../../src/parse.h"
 
+#define OBJECT "Ljava/lang/Object;"
+
 JNIEXPORT jobject JNICALL Java_PDSTools_parse( JNIEnv *env
                                               , jobject obj
                                               , jstring Filename
@@ -13,16 +15,37 @@ JNIEXPORT jobject JNICALL Java_PDSTools_parse( JNIEnv *env
                                               )
 {
   const char* filename = (*env)->GetStringUTFChars(env, Filename, NULL);
-  //FILE* pds = fopen(filename, "rt");
+  FILE* pds = fopen(filename, "rt");
 
-  //if (pds == NULL) {
-  //    return NULL;
-  //}
+  if (pds == NULL) {
+      return NULL;
+  }
 
-  //PDSLabel* label = parse_file(pds);
+  PDSLabel* label = parse_file(pds);
+  fclose(pds);
+  if (label == NULL) {
+      return NULL;
+  }
 
-  //fclose(pds);
-  return blank_map;
+  jclass mapclass = (*env)->FindClass(env, "java/util/HashMap");
+  if (mapclass == NULL) {
+      return NULL;
+  }
+
+  jmethodID jinit = (*env)->GetMethodID(env, mapclass, "<init>", "(I)V");
+  jobject jlabel = (*env)->NewObject(env, mapclass, jinit, 32);
+
+  jmethodID put = (*env)->GetMethodID( env
+                                     , mapclass
+                                     , "put"
+                                     , "(" OBJECT "" OBJECT ")" OBJECT
+  );
+
+  jstring jversion_key = (*env)->NewStringUTF(env, "PDS_VERSION_ID");
+  jstring jversion = (*env)->NewStringUTF(env, label->version);
+  (*env)->CallObjectMethod(env, jlabel, put, jversion_key, jversion);
+
+  return jlabel;
 }
 
 void main() {};
