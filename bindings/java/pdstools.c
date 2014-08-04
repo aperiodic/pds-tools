4aea83a67dc9df31be6e191db321eb2f6639065f
aperiodic <dlp@aperiodic.org>
2014-08-03 19:33:21 -0700

    WIP: first draft of Value to jobject conversion.
    In order to stick the values into the java maps of each PDSObject's
    attributes, we need to wrap the values in java objects.

--- bindings/java/pdstools.c
+++ bindings/java/pdstools.c
@@ -1,18 +1,34 @@
-#include "org_aperiodic_PDSTools.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "../../src/cfg.h"
-#include "../../src/parse.h"
+#include "cfg.h"
+#include "hashtable.h"
+#include "hashtable_itr.h"
+#include "parse.h"
+
+#include "org_aperiodic_PDSTools.h"
 
 #define OBJECT "Ljava/lang/Object;"
+#define MAP_CAPACITY 32
+
+DEFINE_HASHTABLE_SEARCH(ht_search_for_obj, char, PDSObject);
+DEFINE_HASHTABLE_SEARCH(ht_search_for_value, char, Value);
+
+void dump_pdsobj_to_jmap(PDSObject* obj, jobject jmap, JNIEnv *env, jmethodID put);
+jobject pdsvalue_to_jobject(Value* val, JNIEnv* env);
+
+//
+// JNI Interface Implementation
+//
 
 JNIEXPORT jobject JNICALL Java_org_aperiodic_PDSTools_parse( JNIEnv *env
                                                            , jobject obj
                                                            , jstring Filename
                                                            )
 {
+  // Parse the File
   const char* filename = (*env)->GetStringUTFChars(env, Filename, NULL);
   FILE* pds = fopen(filename, "rt");
 
@@ -26,25 +42,135 @@ JNIEXPORT jobject JNICALL Java_org_aperiodic_PDSTools_parse( JNIEnv *env
       return NULL;
   }
 
+  // Do the setup for being able to create & put into hashmaps.
   jclass mapclass = (*env)->FindClass(env, "java/util/HashMap");
   if (mapclass == NULL) {
       return NULL;
   }
 
-  jmethodID jinit = (*env)->GetMethodID(env, mapclass, "<init>", "(I)V");
-  jobject jlabel = (*env)->NewObject(env, mapclass, jinit, 32);
-
+  jmethodID map_init = (*env)->GetMethodID(env, mapclass, "<init>", "(I)V");
   jmethodID put = (*env)->GetMethodID( env
                                      , mapclass
                                      , "put"
                                      , "(" OBJECT "" OBJECT ")" OBJECT
   );
 
+  jobject jlabel = (*env)->NewObject(env, mapclass, map_init, MAP_CAPACITY);
   jstring jversion_key = (*env)->NewStringUTF(env, "PDS_VERSION_ID");
   jstring jversion = (*env)->NewStringUTF(env, label->version);
   (*env)->CallObjectMethod(env, jlabel, put, jversion_key, jversion);
 
+  // stick all the objects in the label map
+  int object_count = hashtable_count(label->objects);
+  if (object_count > 0) {
+      jobject* obj_jmaps = malloc(sizeof(jobject) * object_count);
+      for (int i = 0; i < object_count; i++) {
+          obj_jmaps[i] = (*env)->NewObject(env, mapclass, map_init, MAP_CAPACITY);
+      }
+
+      struct hashtable_itr* it8r = hashtable_iterator(label->objects);
+      char* name;
+      PDSObject* pds_obj;
+      int i = 0;
+      do {
+          name = hashtable_iterator_key(it8r);
+          pds_obj = hashtable_iterator_value(it8r);
+
+          jstring jobj_key = (*env)->NewStringUTF(env, name);
+          dump_pdsobj_to_jmap(pds_obj, obj_jmaps[i], env, put);
+          (*env)->CallObjectMethod(env, jlabel, put, jobj_key, obj_jmaps[i]);
+
+          i++;
+      } while (hashtable_iterator_advance(it8r));
+
+      free(it8r);
+  }
+
   return jlabel;
+}
+
+void dump_pdsobj_to_jmap(PDSObject* obj, jobject jmap, JNIEnv* env, jmethodID put) {
+    jstring jname_key = (*env)->NewStringUTF(env, "ID");
+    jstring jobj_name = (*env)->NewStringUTF(env, obj->name);
+    (*env)->CallObjectMethod(env, jmap, put, jname_key, jobj_name);
+
+    if (hashtable_count(obj->attrs) > 0) {
+        struct hashtable_itr* it8r = hashtable_iterator(obj->attrs);
+        char* attr;
+        Value* value;
+
+        do {
+            attr = hashtable_iterator_key(it8r);
+            jstring jattr = (*env)->NewStringUTF(env, attr);
+
+            value = hashtable_iterator_value(it8r);
+            jobject jvalue = pdsvalue_to_jobject(value, env);
+            (*env)->CallObjectMethod(env, jmap, put, jattr, jvalue);
+
+        } while (hashtable_iterator_advance(it8r));
+
+        free(it8r);
+    }
+}
+
+jobject pdsvalue_to_jobject(Value* val, JNIEnv* env) {
+    if (val->generic.type == CFG_VALUE_TUPLE) {
+        return (jobject) (*env)->NewStringUTF(env, "PDSTools/unsupported");
+    } else {
+        PrimitiveValue pval = val->primitive;
+        jclass formatclass, intclass, floatclass;
+
+        // get a value for jval
+        switch (pval.generic.type) {
+            case CFG_STRING:
+                return (jobject) (*env)->NewStringUTF(env, pval.string.value);
+                break;
+
+            case CFG_DATE:
+                formatclass = (*env)->FindClass(env, "java/text/SimpleDateFormat");
+                jstring ISOFORMAT = (*env)->NewStringUTF(env, "yyyy-MM-dd'T'HH:mm:zz");
+                if (formatclass == NULL) {
+                    return (jobject) (*env)->NewStringUTF(env, "PDSTools/noSimpleDateFormatClass");
+                }
+                jmethodID isof_init = (*env)->GetMethodID( env
+                                                         , formatclass
+                                                         , "<init>"
+                                                         , "(Ljava/lang/String;)V"
+                                                         );
+
+                // could very well segfault here
+                jobject isof = (*env)->NewObject(env, formatclass, isof_init, ISOFORMAT);
+                jmethodID isof_parse = (*env)->GetMethodID( env
+                                                          , formatclass
+                                                          , "parse"
+                                                          , "(Ljava/lang/String;)Ljava/util/Date;"
+                                                          );
+                jstring date_jstring = (*env)->NewStringUTF(env, pval.date.value);
+                return (*env)->CallObjectMethod(env, isof, isof_parse, date_jstring);
+                break;
+
+            case CFG_NUMBER_INTEGRAL:
+                intclass = (*env)->FindClass(env, "java/lang/Integer");
+                if (intclass == NULL) {
+                    return (jobject) (*env)->NewStringUTF(env, "PDSTools/noIntegerClass");
+                }
+                jmethodID jint_init = (*env)->GetMethodID(env, intclass, "<init>", "(I)V");
+                return (*env)->NewObject(env, intclass, jint_init, pval.integer.type);
+                break;
+
+            case CFG_NUMBER_RATIONAL:
+                floatclass = (*env)->FindClass(env, "java/lang/Float");
+                if (floatclass == NULL) {
+                    return (jobject) (*env)->NewStringUTF(env, "PDSTools/noIntegerClass");
+                }
+                jmethodID jfloat_init = (*env)->GetMethodID(env, floatclass, "<init>", "(F)V");
+                return (*env)->NewObject(env, floatclass, jfloat_init, pval.rational.value);
+                break;
+
+            default:
+                return (jobject) (*env)->NewStringUTF(env, "PDSTools/unsupported");
+        }
+    }
 }
 
 void main() {};
