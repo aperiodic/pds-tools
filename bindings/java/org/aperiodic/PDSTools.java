f37885d842ba9c8e1dd50a77277a981a570bc0ea
aperiodic <dlp@aperiodic.org>
2014-08-23 22:57:15 -0700

    Actually construct PDSLabel in java bindings.
    Add a method to the java JNI bindings' class that uses the map returned
    by the native method to construct a PDSLabel instance.
    
    In the JNI bindings' main, use the above method to make a PDSLabel
    object and print out all of its metadata and objects.

--- bindings/java/org/aperiodic/PDSTools.java
+++ bindings/java/org/aperiodic/PDSTools.java
@@ -5,6 +5,26 @@ import java.util.LinkedHashMap;
 
 public class PDSTools {
   public native Map<String,Object> parse(String filename);
+  private static boolean loaded = false;
+
+  public static PDSLabel parseLabel(String filename) {
+    if (!loaded) {
+      System.loadLibrary("pdstools");
+      loaded = true;
+    }
+
+    PDSTools jni_wrapper = new PDSTools();
+    Map<String,Object> mlabel = jni_wrapper.parse(filename);
+
+    if (mlabel == null) {
+      return null;
+    }
+
+    String version = (String) mlabel.get("PDS_VERSION_ID");
+    Map<String,Object> metadata = (Map<String,Object>) mlabel.get("METADATA");
+    Map<String,Map<String,Object>> objects = (Map<String,Map<String,Object>>) mlabel.get("OBJECTS");
+    return new PDSLabel(version, metadata, objects);
+  }
 
   public static void main(String[] args) {
     String label_name;
@@ -14,16 +34,31 @@ public class PDSTools {
       label_name = "samples/mola.lbl";
     }
 
-    System.out.println(System.getProperty("java.library.path"));
-    System.loadLibrary("pdstools");
-    PDSTools pds_tools = new PDSTools();
-    Map<String,Object> mlabel = pds_tools.parse(label_name);
+    PDSLabel label = parseLabel(label_name);
 
-    if (mlabel == null) {
+    if (label == null) {
       System.out.println("couldn't parse " + label_name);
       return;
     }
 
-    System.out.println("parsed a version " + mlabel.get("PDS_VERSION_ID") + " label from " + label_name);
+    System.out.println("parsed a version " + label.getVersion() + " label from " + label_name);
+    System.out.println();
+
+    for (Map.Entry<String,PDSValue> datum : label.metadata.entrySet()) {
+      String key = datum.getKey();
+
+      PDSValue value = datum.getValue();
+      System.out.println(key + ": " + value.toString());
+    }
+    System.out.println();
+
+    for (Map.Entry<String,PDSObject> obj_entry : label.objects.entrySet()) {
+      String name = obj_entry.getKey();
+      System.out.println(name + " {");
+      for (Map.Entry<String,PDSValue> attr : obj_entry.getValue().attributes.entrySet()) {
+        System.out.println("  " + attr.getKey() + ": " + attr.getValue().toString());
+      }
+      System.out.println("}\n");
+    }
   }
 }
